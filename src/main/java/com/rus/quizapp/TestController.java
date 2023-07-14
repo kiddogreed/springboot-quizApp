884252ae1bd1b541251ed743d565d97f6d2f3779
kiddo <johnrusselledomingo@gmail.com>
2023-07-14 23:26:12 +0800

    continue building spring boot

--- src/main/java/com/rus/quizapp/TestController.java
+++ src/main/java/com/rus/quizapp/TestController.java
@@ -1,4 +1,8 @@
+package com.rus.quizapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test")
